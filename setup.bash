e7b52b51ce6fcad5c6b91a7d6b150067eeaeff57
KC-Zhang <kc.zhang.ut@gmail.com>
2020-05-27 11:20:13 +0800

    allow host

--- setup.bash
+++ setup.bash
@@ -1,3 +1,6 @@
+# add server ip to settings.py, i.e ALLOWED_HOSTS = ['34.92.69.244']
+# run server with python3 site_SMB/manage.py runserver 0.0.0.0:8000
+
 sudo apt update
 sudo apt install python3-pip
 alias python=python3
