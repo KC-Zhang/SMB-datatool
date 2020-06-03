77e1b8614ce5c4ac648f6c1c45b6d889572e519e
KC-Zhang <kc.zhang.ut@gmail.com>
2020-06-04 01:07:38 +0800

    add setup.bash

--- setup.bash
+++ setup.bash
@@ -10,7 +10,19 @@ pip install pandas
 pip install xlrd
 pip install openpyxl
 pip install XlsxWriter
+pip install django-cleanup
 
+# for making icons
 sudo snap install inkscape
+
+# for converting downloaded html landing page to django template
+#regex search: =("|')(.+?)(?<=tony-ai-1)  #change tony-ai-1 accordingly
+#regex replace: =$1./landingPage_files
+#replace //(.+?)(?<=tony-ai-1)
+# by ./landingPage_files
+#replace (src|href)="(?!(http|#))(.+?)(?<!html)"
+#by $1="{% static '$3' %}"
+#replace (url\()(.+?)(\))
+#by $1{% static '$2' %}$3
 
 
