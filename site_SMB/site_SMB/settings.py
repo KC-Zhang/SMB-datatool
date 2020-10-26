b095bb302a8749f9aa4a4fc2361c352804542ccf
KC-Zhang <420126360@qq.com>
2020-10-26 16:47:46 -0400

    Update settings.py

--- site_SMB/site_SMB/settings.py
+++ site_SMB/site_SMB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yq_ht+8@irxw9o(sn$u!nl%syd27)4fk#+#lh^^p2**k-$te=n'
+SECRET_KEY = # run following command in terminal of your machine to get secret key: python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
